56a4c0b7915016fbb4a7a13805be5fffde169e0b
Alternativy <vesdsdsesfaeaw@gmail.com>
2021-03-19 22:36:03 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var hola = "hola mundo"
+Declarar variables:
+let x = 1
+constante, no varia
+const y = 2
+Variable antigua, sirve de todo
+var z = 3
+nada y var es lo mismo
+let en este contexto
+no se debe repetir en otro contexto
+
+// se puede usar la primera we
+let age= 30
+let string = "edad"+ age;
+// ctrl + Ç para tirar comentarios`
+// jaja no tengo idea de que estoy haciendo
